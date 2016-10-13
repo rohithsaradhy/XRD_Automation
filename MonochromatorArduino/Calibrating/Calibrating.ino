801954ed6a57c564359266c0a63061cc1430415c
Rohith Saradhy <rohithsaradhy@gmail.com>
2016-10-13 22:40:04 +0530

    final

--- MonochromatorArduino/Calibrating/Calibrating.ino
+++ MonochromatorArduino/Calibrating/Calibrating.ino
@@ -3,9 +3,9 @@ int inp=0,motor;
 long start_time,stop_time;
 void setup(){
   //Setting up the pins for controlling the table
-  pinMode(PushBtn,OUTPUT);//RY1 & RY3
+  pinMode(PushBtn,OUTPUT);//RY4
   pinMode(Motor_selector,OUTPUT);//RY2
-  pinMode(FlipSwt,OUTPUT);//RY4
+  pinMode(FlipSwt,OUTPUT);//RY1 & RY3
   digitalWrite(Motor_selector,0);
   digitalWrite(FlipSwt,0);
   digitalWrite(PushBtn,0);
